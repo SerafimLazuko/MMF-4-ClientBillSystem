68769962935253025a6e875bd9364d669c22af95
Serafim Lazuka <Serafim_Lazuka@epam.com>
2020-02-20 18:01:27 +0300

    add fields to payment model

--- ClientBillWebApp/ClientBillWebApp/Models/PaymentModel.cs
+++ ClientBillWebApp/ClientBillWebApp/Models/PaymentModel.cs
@@ -5,7 +5,12 @@ using System.Threading.Tasks;
 
 namespace ClientBillWebApp.Models
 {
-    public class Payment
+    public class PaymentModel
     {
+        public Guid PaymentId { get; set; }
+        public Guid DestinationBillId { get; set; }
+        public Guid SourceBillId { get; set; }
+        public double TransferAmount { get; set; }
+        public DateTime DateTime { get; set; }
     }
 }
