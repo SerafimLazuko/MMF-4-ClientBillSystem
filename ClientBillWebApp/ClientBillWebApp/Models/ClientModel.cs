6c8bea2b4b4da0cbb785d21288040f9191d89307
Serafim Lazuka <Serafim_Lazuka@epam.com>
2020-02-20 17:38:37 +0300

    Add Client

---
+++ ClientBillWebApp/ClientBillWebApp/Models/ClientModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClientBillWebApp.Models
+{
+    public class ClientModel
+    {
+        public string Name { get; set; }
+
+        public int ClientId { get; set; }
+        
+        public ClientModel()
+        {
+
+        }
+
+    }
+}
